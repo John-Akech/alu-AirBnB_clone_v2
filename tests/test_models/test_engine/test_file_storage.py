a3cc15d79ab94905a4e01a76bcab78ed0f41d085
John Deng Madit Akech <127344777+John-Akech@users.noreply.github.com>
2024-01-29 17:38:54 +0200

    Update test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -8,7 +8,7 @@ import os
 
 
 @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db', "skip if not db")
-class test_fileStorage(unittest.TestCase):
+class TestFileStorage(unittest.TestCase):
     """ Class to test the file storage method """
 
     def setUp(self):
@@ -26,6 +26,7 @@ class test_fileStorage(unittest.TestCase):
             os.remove('file.json')
         except:
             pass
+
         del self.storage
 
     def test_obj_list_empty(self):
@@ -104,7 +105,7 @@ class test_fileStorage(unittest.TestCase):
         _id = new.to_dict()['id']
         for key in self.storage.all().keys():
             temp = key
-            self.assertEqual(temp, 'BaseModel' + ',' + _id)
+            self.assertEqual(temp, 'BaseModel' + '.' + _id)
 
     def test_storage_var_created(self):
         """ FileStorage object storage created """
