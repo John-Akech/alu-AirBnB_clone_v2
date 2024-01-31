46950d35c21fe65fc27d05574ab2377ac58fee15
John Deng Madit Akech <127344777+John-Akech@users.noreply.github.com>
2024-01-31 14:06:31 +0200

    Update test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,36 +1,39 @@
 #!/usr/bin/python3
-""" """
+"""Unit tests for Review class"""
+
 from models.place import Place
 from models.user import User
 from tests.test_models.test_base_model import TestBaseModel
 from models.review import Review
 
-
 class TestReview(TestBaseModel):
-    """Test for review """
+    """Test Review class"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Initialize TestReview instance"""
         super().__init__(*args, **kwargs)
         self.name = "Review"
         self.value = Review
 
-    def test_place_id(self):
-        """ """
-        new = self.value()
+    def test_place_id_attribute(self):
+        """Test setting the place_id attribute of Review"""
+        new_review = self.value()
         place = Place()
-        new.place_id = place.id
-        self.assertEqual(type(new.place_id), str)
+        new_review.place_id = place.id
+        self.assertEqual(type(new_review.place_id), str)
 
-    def test_user_id(self):
-        """ """
-        new = self.value()
+    def test_user_id_attribute(self):
+        """Test setting the user_id attribute of Review"""
+        new_review = self.value()
         user = User()
-        new.user_id = user.id
-        self.assertEqual(type(new.user_id), str)
+        new_review.user_id = user.id
+        self.assertEqual(type(new_review.user_id), str)
 
-    def test_text(self):
-        """ """
-        new = self.value()
-        new.text = ""
-        self.assertEqual(type(new.text), str)
+    def test_text_attribute(self):
+        """Test setting the text attribute of Review"""
+        new_review = self.value()
+        new_review.text = ""
+        self.assertEqual(type(new_review.text), str)
+
+if __name__ == '__main__':
+    unittest.main()
