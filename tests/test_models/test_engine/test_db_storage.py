bb16a52e93cfd3f384aeabd2524f950a9fe0f087
John Deng Madit Akech <127344777+John-Akech@users.noreply.github.com>
2024-02-04 18:17:37 +0200

    Update test_db_storage.py

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import unittest
-import models
+from models import storage
 from models.user import User
 from models.review import Review
 from models.amenity import Amenity
@@ -8,6 +8,7 @@ from models.state import State
 from models.place import Place
 from models.city import City
 import os
+from datetime import datetime
 
 
 # skip these tests if db is not the storage
@@ -17,85 +18,106 @@ class TestDBStorage(unittest.TestCase):
 
     def setUp(self):
         """Set up the test environment"""
-        self.storage = models.storage
+        self.storage = storage
+        self.session = storage._DBStorage__session
 
     def tearDown(self):
         """Remove storage file at the end of tests"""
+        self.session.rollback()
+        self.session.close()
         del self.storage
 
     def test_user(self):
         """Test User"""
         user = User(name="Brian", email="brian@gmail.com", password="Brian123")
-        user.save()
-        self.assertIn(user.id, self.storage.all())
+        self.session.add(user)
+        self.session.commit()
+        self.assertIn(user.id, self.storage.all(User))
         self.assertEqual(user.name, "Brian")
 
     def test_city(self):
         """Test City"""
         state = State(name="Kenya")
-        state.save()
+        self.session.add(state)
+        self.session.commit()
+
         city = City(name="Nairobi")
         city.state_id = state.id
-        city.save()
-        self.assertIn(city.id, self.storage.all())
+        self.session.add(city)
+        self.session.commit()
+
+        self.assertIn(city.id, self.storage.all(City))
         self.assertEqual(city.name, "Nairobi")
 
     def test_state(self):
         """Test State"""
         state = State(name="Kenya")
-        state.save()
-        self.assertIn(state.id, self.storage.all())
+        self.session.add(state)
+        self.session.commit()
+
+        self.assertIn(state.id, self.storage.all(State))
         self.assertEqual(state.name, "Kenya")
 
     def test_place(self):
         """Test Place"""
         state = State(name="Kenya")
-        state.save()
+        self.session.add(state)
+        self.session.commit()
 
         city = City(name="Nairobi")
         city.state_id = state.id
-        city.save()
+        self.session.add(city)
+        self.session.commit()
 
         user = User(name="Brian", email="brian@gmail.com", password="Brian123")
-        user.save()
+        self.session.add(user)
+        self.session.commit()
 
         place = Place(name="PentHouse", number_rooms=4)
         place.city_id = city.id
         place.user_id = user.id
-        place.save()
+        self.session.add(place)
+        self.session.commit()
 
-        self.assertIn(place.id, self.storage.all())
+        self.assertIn(place.id, self.storage.all(Place))
         self.assertEqual(place.number_rooms, 4)
         self.assertEqual(place.name, "PentHouse")
 
     def test_amenity(self):
         """Test Amenity"""
         amenity = Amenity(name="Spoon")
-        amenity.save()
-        self.assertIn(amenity.id, self.storage.all())
+        self.session.add(amenity)
+        self.session.commit()
+
+        self.assertIn(amenity.id, self.storage.all(Amenity))
         self.assertEqual(amenity.name, "Spoon")
 
     def test_review(self):
         """Test Review"""
         state = State(name="Kenya")
-        state.save()
+        self.session.add(state)
+        self.session.commit()
 
         city = City(name="Nairobi")
         city.state_id = state.id
-        city.save()
+        self.session.add(city)
+        self.session.commit()
 
         user = User(name="Brian", email="brian@gmail.com", password="Brian123")
-        user.save()
+        self.session.add(user)
+        self.session.commit()
 
         place = Place(name="PentHouse", number_rooms="4")
         place.city_id = city.id
         place.user_id = user.id
-        place.save()
+        self.session.add(place)
+        self.session.commit()
 
         review = Review(text="work smart", place_id=place.id, user_id=user.id)
-        review.save()
+        self.session.add(review)
+        self.session.commit()
 
-        self.assertIn(review.id, self.storage.all())
+        self.assertIn(review.id, self.storage.all(Review))
         self.assertEqual(review.text, "work smart")
 
 
