7d596180c01d8c60fa66e237dfd7edad9e95a186
John Deng Madit Akech <127344777+John-Akech@users.noreply.github.com>
2024-02-15 13:36:38 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,40 +1,27 @@
 #!/usr/bin/python3
-""" Function that compress a folder """
+"""Fabric script to generate a .tgz archive"""
+
+from fabric.api import local
 from datetime import datetime
-from fabric.api import *
-import shlex
-import os
 
+def do_pack():
+    """Generates a .tgz archive from the contents of the web_static folder"""
 
-env.hosts = ['35.231.33.237', '34.74.155.163']
-env.user = "ubuntu"
+    # Create the 'versions' folder if it doesn't exist
+    local("mkdir -p versions")
 
+    # Get the current date and time
+    now = datetime.now()
+    timestamp = now.strftime("%Y%m%d%H%M%S")
 
-def do_deploy(archive_path):
-    """ Deploys """
-    if not os.path.exists(archive_path):
-        return False
-    try:
-        name = archive_path.replace('/', ' ')
-        name = shlex.split(name)
-        name = name[-1]
+    # Name of the archive
+    archive_name = "web_static_{}.tgz".format(timestamp)
 
-        wname = name.replace('.', ' ')
-        wname = shlex.split(wname)
-        wname = wname[0]
+    # Compress the contents of the web_static folder into a .tgz archive
+    result = local("tar -cvzf versions/{} web_static".format(archive_name))
 
-        releases_path = "/data/web_static/releases/{}/".format(wname)
-        tmp_path = "/tmp/{}".format(name)
-
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
-        print("New version deployed!")
-        return True
-    except:
-        return False
+    # Check if the compression was successful
+    if result.failed:
+        return None
+    else:
+        return "versions/{}".format(archive_name)
