1a7f93c22c8ec534c648f0bd624882fbd8184337
John Deng Madit Akech <127344777+John-Akech@users.noreply.github.com>
2024-02-16 12:25:17 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,10 @@
-from fabric.api import env, put, run, task
+#!/usr/bin/python3
+# script that distributes an archive to web servers
+from fabric.api import env, put, run, local
 from os.path import exists, isdir
 import os.path
 import re
+
 
 # Set the username and host for SSH connection to the server
 env.user = 'ubuntu'
@@ -11,18 +14,19 @@ env.key_filename = '~/.ssh/id_rsa'
 
 def do_deploy(archive_path):
     """
-    Distributes archive to web servers
+        Distributes archive to web servers
     """
     # Check if the archive file exists
     if not exists(archive_path):
-        raise Exception("Archive does not exist.")
+        return False
 
     # Upload the archive to the /tmp/ directory of the web server
     put(archive_path, "/tmp/")
 
     # Uncompress the archive to the folder
     filename = re.search(r'[^/]+$', archive_path).group(0)
-    folder = "/data/web_static/releases/{}".format(os.path.splitext(filename)[0])
+    folder = "/data/web_static/releases/{}".format(
+        os.path.splitext(filename)[0])
 
     # Create the folder if it doesn't exist
     if not exists(folder):
@@ -46,23 +50,15 @@ def do_deploy(archive_path):
     # Create new symbolic link
     run("ln -s {} /data/web_static/current".format(folder))
 
-
-@task
-def synchronize_files(c):
-    """
-    Synchronize files from web_static to web servers
-    """
     # Create 'hbnb_static' directory if it doesn't exist
     if not isdir("/var/www/html/hbnb_static"):
-        c.sudo("mkdir -p /var/www/html/hbnb_static")
+        run("sudo mkdir -p /var/www/html/hbnb_static")
 
-    # Sync 'hbnb_static' with 'current' on each server
-    for host in env.hosts:
-        c.sudo("rsync -avz --delete /data/web_static/current/ /var/www/html/hbnb_static/", user="root")
+    # Sync 'hbnb_static' with 'current'
+    run("sudo cp -r /data/web_static/current/* /var/www/html/hbnb_static/")
 
-    print("Files synchronized successfully.")
-
+    print("New version deployed!")
+    return True
 
 # Usage:
-# fab -f deploy.py do_deploy:/path/to/file.tgz
-# fab -f deploy.py synchronize_files
+# fab -f 2-do_deploy_web_static.py do_deploy:/path/to/file.tgz
