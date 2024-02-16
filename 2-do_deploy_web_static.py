82eb47cce1f49e3a4b6fe5e2966c71260d94e24a
John Deng Madit Akech <127344777+John-Akech@users.noreply.github.com>
2024-02-16 21:47:48 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,49 +1,69 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
-import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+"""Fabric script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers,
+using the function do_deploy
+"""
+import time
+import os
+from fabric.api import *
+from fabric.operations import run, put
 
-env.hosts = ["54.197.135.244", "54.167.3.103"]
+
+env.hosts = ['54.197.135.244', '54.167.3.103']
+env.user = 'ubuntu'
+
+
+def do_pack():
+    """generates a .tgz archive"""
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{:s}.tgz web_static/".
+              format(time.strftime("%Y%m%d%H%M%S")))
+        return "versions/web_static_{:s}.tgz".\
+            format(time.strftime("%Y%m%d%H%M%S"))
+    except BaseException:
+        return None
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
+    """distributes an archive to my web servers"""
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        # Upload archive
+        put(archive_path, '/tmp/')
 
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
-    """
-    if os.path.isfile(archive_path) is False:
-        return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
+        # Create a target dir without the file extension
+        timestamp = time.strftime("%Y%m%d%H%M%S")
+        run(
+            'sudo mkdir -p /data/web_static/releases/web_static_{:s}/'.
+            format(timestamp))
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        # uncompress archive to the targed dir
+        run('sudo tar xzvf /tmp/web_static_{:s}.tgz --directory\
+            /data/web_static/releases/web_static_{:s}/'.
+            format(timestamp, timestamp))
+
+        # delete the archive from the web server
+        run('sudo rm /tmp/web_static_{:s}.tgz'.format(timestamp))
+
+        # move contents into host web_static
+        run('sudo mv /data/web_static/releases/web_static_{:s}/web_static/*\
+            /data/web_static/releases/web_static_{}/'.format(
+            timestamp, timestamp))
+
+        # remove irrelevant web_static dir
+        run('sudo rm -rf /data/web_static/releases/web_static_{}/web_static'.
+            format(timestamp))
+
+        # delete the initial symbolic link from the web server
+        run('sudo rm -rf /data/web_static/current')
+
+        # create a new symbolic link
+        run('sudo ln -s /data/web_static/releases/web_static_{:s}/ \
+            /data/web_static/current'.format(
+            timestamp))
+    except BaseException:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-        return False
+    # if all that succeeded, return True
     return True
