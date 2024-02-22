6aa0139be510a4736ed588e4c2d20a4148e503ec
Skomaiya <s.komaiya@alustudent.com>
2024-02-22 07:17:03 +0000

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,9 +3,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
+@app.route("/")
 def index():
+    """
+    Route for the root URL ("/").
+    
+    Returns:
+        str: A message for the index page.
+    """
     return "Hello HBNB!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
