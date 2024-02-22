da2c7066060a0e15d167cb7e8b1dba3df64daac5
Skomaiya <s.komaiya@alustudent.com>
2024-02-22 11:56:33 +0000

    task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -20,91 +20,49 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def index():
-    """
-    Route for the root URL ("/").
-    
-    Returns:
-        str: A message for the index page.
-    """
+    """Route for the root URL ("/")."""
     return "Hello HBNB!"
-    
+
+
 @app.route("/hbnb", strict_slashes=False)
 def about():
-    """
-    Route for /hbnb URL.
-    
-    Returns:
-        str: Message for the about page.
-    """
+    """Route for /hbnb URL."""
     return "HBNB"
-    
+
+
 @app.route("/c/<text>", strict_slashes=False)
 def C(text):
-    """
-    Route for /c/<text> URL.
-    
-    Args:
-        text (str): Text parameter passed in the URL.
-    
-    Returns:
-        str: Message with "C " followed by the value of the text variable.
-    """
+    """Route for /c/<text> URL."""
     return "C %s" % text.replace('_', ' ')
 
-@app.route("/python")
-@app.route("/python/(<text>)")
-def python(text = "is cool"):
-    """
-    Route for /python URL.
-    
-    Args:
-        text (str, optional): Text parameter passed in the URL. Defaults to "is cool".
-    
-    Returns:
-        str: Message with "Python " followed by the value of the text variable.
-    """
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Route for /python URL."""
     return "Python %s" % text.replace('_', ' ')
-    
+
+
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
-    """
-    Route for /number/<n> URL.
-    
-    Args:
-        n (int): Integer parameter passed in the URL.
-    
-    Returns:
-        str: Message indicating that the passed number is a number.
-    """
+    """Route for the /number/<n> URL."""
     return "%d is a number" % n
+
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
-    """
-    Route for /number_template/<n> URL.
-    
-    Args:
-        n (int): Integer parameter passed in the URL.
-    
-    Returns:
-        str: Rendered HTML template with the provided number.
-    """
-    return render_template("5-number.html")
+    """Route for the /number_template/<n> URL."""
+    return render_template("5-number.html", n=n)
+
 
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
-    """
-    Route for /number_odd_or_even/<n> URL.
-    
-    Args:
-        n (int): Integer parameter passed in the URL.
-    
-    Returns:
-        str: Rendered HTML template with information about whether the number is odd or even.
-    """
+    """Route for the /number_odd_or_even/<n> URL."""
     return render_template("6-number_odd_or_even.html", n=n)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
