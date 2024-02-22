f4dfbb7abd810a92ac6babcd7e21eda8dc14f649
Skomaiya <s.komaiya@alustudent.com>
2024-02-22 11:44:15 +0000

    task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -6,7 +6,7 @@ It defines four routes:
 - Route for the root URL ("/") that displays "Hello HBNB!"
 - Route for "/hbnb" that displays "HBNB"
 - Route for "/c/<text>" that displays "C <text>" where <text> can be any string
-- Route for "/python/(<text>)" that displays "Python <text>" where <text> is a parameter that defaults to "is cool"
+- Route for "/python/(<text>)" that displays "Python <text>"
 - Route for the "/number/<n>" URL.
 
 Requirements:
@@ -21,67 +21,32 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """
-    Route for the root URL ("/").
-    
-    Returns:
-        str: A message for the index page.
-    """
+    """Route for the root URL ("/")."""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def display_hbnb():
-    """
-    Route for the "/hbnb" URL.
-    
-    Returns:
-        str: A message for the HBNB page.
-    """
+    """Route for the "/hbnb" URL."""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def display_c(text):
-    """
-    Route for the "/c/<text>" URL.
-    
-    Args:
-        text (str): The text to display after "C ".
-    
-    Returns:
-        str: A message for the "C" page with the provided text.
-    """
+    """Route for the "/c/<text>" URL."""
     return "C %s" % text.replace('_', ' ')
 
 
 @app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def display_python(text):
-    """
-    Route for the "/python/" and "/python/<text>" URLs.
-    
-    Args:
-        text (str): The text to display after "Python ".
-    
-    Returns:
-        str: A message for the "Python" page with the provided text.
-    """
+    """Route for the "/python/" and "/python/<text>" URLs."""
     return "Python %s" % text.replace('_', ' ')
 
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def display_number(n):
-    """
-    Route for the "/number/<n>" URL.
-    
-    Args:
-        n (int): The number to check.
-    
-    Returns:
-        str: A message indicating whether the provided value is a number.
-            If it's a number, it returns "n is a number"; otherwise, it returns None.
-    """
+    """Route for the "/number/<n>" URL."""
     return "%d is a number" % n
 
 
