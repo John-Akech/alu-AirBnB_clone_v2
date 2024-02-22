4fa89123c1f4208307cbc7dc3b3e4c35c4e3eff1
Skomaiya <s.komaiya@alustudent.com>
2024-02-22 09:19:10 +0000

    task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -15,10 +15,10 @@ Requirements:
 - Your web application must be listening on 0.0.0.0, port 5000
 """
 
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def index():
@@ -29,21 +29,23 @@ def index():
         str: A message for the index page.
     """
     return "Hello HBNB!"
-    
+
+
 @app.route("/hbnb", strict_slashes=False)
-def about():
+def hbnb():
     """
-    Route for /hbnb URL.
+    Route for the /hbnb URL.
     
     Returns:
         str: A message for the HBNB page.
     """
     return "HBNB"
-    
+
+
 @app.route("/c/<text>", strict_slashes=False)
-def C(text):
+def c(text):
     """
-    Route for /c/<text> URL.
+    Route for the /c/<text> URL.
     
     Args:
         text (str): Text to display after "C".
@@ -53,24 +55,26 @@ def C(text):
     """
     return "C %s" % text.replace('_', ' ')
 
-@app.route("/python")
-@app.route("/python/(<text>)")
-def python(text="is cool"):
+
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text):
     """
-    Route for /python and /python/<text> URLs.
+    Route for the /python and /python/<text> URLs.
     
     Args:
-        text (str, optional): Text to display after "Python". Defaults to "is cool".
+        text (str): Text to display after "Python".
     
     Returns:
         str: A message including the text provided in the URL.
     """
     return "Python %s" % text.replace('_', ' ')
 
+
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
     """
-    Route for /number/<n> URL.
+    Route for the /number/<n> URL.
     
     Args:
         n (int): Number to display in the URL.
@@ -78,20 +82,22 @@ def number(n):
     Returns:
         str: A message indicating whether n is a number or not.
     """
-    return "%d is a number" % n if isinstance(n, int) else None
+    return "%d is a number" % n
 
-app.route("/number_template/<int:n>", strict_slashes=False)
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
     """
-    Route for /number_template/<n> URL.
+    Route for the /number_template/<n> URL.
     
     Args:
-        n (str): Text to be passed to the HTML template.
+        n (int): Number to display in the HTML template.
     
     Returns:
-        str: Rendered HTML template.
+        str: Rendered HTML template with the number.
     """
-    return render_template("5-number.html")
+    return render_template("5-number.html", n=n)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
