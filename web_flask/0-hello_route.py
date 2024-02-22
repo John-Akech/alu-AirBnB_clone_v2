fa5683696e714dee9421198f4611b4e8241716de
Skomaiya <s.komaiya@alustudent.com>
2024-02-22 07:25:17 +0000

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
+"""
+This is a simple Flask web application.
+
+It defines a single route for the root URL ("/") and displays "Hello HBNB!".
+
+Requirements:
+- Your web application must be listening on 0.0.0.0, port 5000
+- You must use the option strict_slashes=False in your route definition
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def index():
     """
     Route for the root URL ("/").
     
     Returns:
-        str: A message for the index page.
+        str: A greeting message.
     """
     return "Hello HBNB!"
 
