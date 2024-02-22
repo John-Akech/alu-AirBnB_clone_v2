cb3bce9dcf8c13cf7c74bd2dbdc384df12f096cd
Skomaiya <s.komaiya@alustudent.com>
2024-02-22 07:30:27 +0000

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -13,15 +13,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def index():
     """
-    Route for the root URL ("/").
-    
+    Route for the root URL ("/").    
     Returns:
         str: A greeting message.
     """
     return "Hello HBNB!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
