881eb53e7527fe5eed91ccfcfaa393ce45b8bede
John Deng Madit Akech <127344777+John-Akech@users.noreply.github.com>
2024-02-23 09:53:27 +0200

    Update 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """Starts a Flask web application.
 
 The application listens on 0.0.0.0, port 5000.
@@ -7,38 +6,18 @@ Routes:
     /: Displays 'Hello HBNB!'.
     /hbnb: Displays 'HBNB'.
 """
-=======
-"""
-This is a simple Flask web application.
-
-It defines two routes:
-- Route for the root URL ("/") that displays "Hello HBNB!"
-- Route for "/hbnb" that displays "HBNB"
-
-Requirements:
-- Your web application must be listening on 0.0.0.0, port 5000
-"""
-
-
->>>>>>> 426e2ecf714ea63d3944996a4621bdb040eedfc6
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-<<<<<<< HEAD
 def hello_hbnb():
     """Displays 'Hello HBNB!'."""
-=======
-def index():
-    """Route for the root URL ("/")."""
->>>>>>> 426e2ecf714ea63d3944996a4621bdb040eedfc6
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
-<<<<<<< HEAD
 def hbnb():
     """Displays 'HBNB'."""
     return "HBNB"
@@ -46,11 +25,3 @@ def hbnb():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-=======
-def about():
-    """Route for "/hbnb"."""
-    return "HBNB"
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
->>>>>>> 426e2ecf714ea63d3944996a4621bdb040eedfc6
